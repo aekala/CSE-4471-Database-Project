c8ed1004a34aebb06793fabc208e2d667d1ecfb1
Leo <lkkodish@gmail.com>
2019-11-26 15:08:51 -0500

    added ReactTable headers

--- marriott/src/App.js
+++ marriott/src/App.js
@@ -23,13 +23,24 @@ class App extends React.Component {
 
   render() {
     console.log(this.state.data);
-      const data = [{
-      firstname: 'Tanner',
-      lastname: 'Linsely',
-    },{
-      firstname: 'Leo',
-      lastname: 'Kodish'
-    }]
+    let data = [];
+    for (let i = 0; i < this.state.data.length; i++) {
+      const person = this.state.data[i];
+      data[i] = {
+        firstname: person.FirstName,
+        lastname: person.LastName,
+        streetaddress: person.StreetAddress,
+        city: person.City,
+        state: person.State,
+        zipcode: person.ZipCode,
+        birthdate: person.BirthDate,
+        member: person.Member,
+        homenumber: person.HomePhoneNumber,
+        cellnumber: person.CellNumber,
+        passportnumber: person.PassportNumber,
+        creditcardnumber: person.CreditCardNumber,
+      }
+    }
 
     const columns = [{
       Header: 'First Name',
@@ -37,6 +48,36 @@ class App extends React.Component {
     }, {
       Header: 'Last Name',
       accessor: 'lastname',
+    }, {
+      Header: 'Street Address',
+      accessor: 'streetaddress',
+    }, {
+      Header: 'City',
+      accessor: 'city',
+    }, {
+      Header: 'State',
+      accessor: 'state',
+    }, {
+      Header: 'Zip Code',
+      accessor: 'zipcode',
+    }, {
+      Header: 'Birth Date',
+      accessor: 'birthdate',
+    }, {
+      Header: 'Member',
+      accessor: 'member',
+    }, {
+      Header: 'Home Number',
+      accessor: 'homenumber',
+    }, {
+      Header: 'Cell Number',
+      accessor: 'cellnumber',
+    }, {
+      Header: 'Passport Number',
+      accessor: 'passportnumber',
+    }, {
+      Header: 'Credit Card Number',
+      accessor: 'creditcardnumber',
     }]
 
     return <ReactTable data={data} columns={columns} />
